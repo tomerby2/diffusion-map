ccbc65febb2bbb96fddecd2945824ae550df1a45
tomerby2 <tomerby2@gmail.com>
2017-04-02 14:32:21 +0300

    only two's

--- DifusionMapTest.m
+++ DifusionMapTest.m
@@ -2,14 +2,15 @@ close all;
 clc;
 clear;
 images = loadMNISTImages('train-images.idx3-ubyte');
+labels = loadMNISTLabels('train-labels.idx1-ubyte');
 N=100;
-s_images=images(:,1:N);
-%[~,N]=size(images);
-display_network(images(:,1:100)); % Show the first 100 images
+lable2 = find(labels==2,N);
+s_images=images(:,lable2);
+display_network(s_images(:,1:100)); % Show the first 100 images
 Xi=repelem(s_images,1,N);
 Xj=repmat(s_images,1,N);
 NORMS=vec2mat(sum((Xi-Xj).^2,1),N);
-epsilon=20;
+epsilon=1200;
 K=exp(-NORMS/epsilon);
 D=diag(K*ones(N,1));
 A=(inv(D))*K;
@@ -17,4 +18,7 @@ A=(inv(D))*K;
 t=1;
 psi=(EigVal.^t)*(EigVec');
 figure;
+scatter3(EigVec(:,2),EigVec(:,3),EigVec(:,4));
+figure;
 scatter(EigVec(:,2),EigVec(:,3));
+
