8c962596d696af0f8708a0dc2ea23f92dcc7760b
oryair <oryair36@gmail.com>
2017-04-02 15:22:47 +0300

    Some Cosmetics on code (Remove ASV file)

--- DifusionMapTest.m
+++ DifusionMapTest.m
@@ -1,24 +1,36 @@
 close all;
-clc;
 clear;
+
 images = loadMNISTImages('train-images.idx3-ubyte');
 labels = loadMNISTLabels('train-labels.idx1-ubyte');
-N=100;
-lable2 = find(labels==2,N);
-s_images=images(:,lable2);
-display_network(s_images(:,1:100)); % Show the first 100 images
-Xi=repelem(s_images,1,N);
-Xj=repmat(s_images,1,N);
-NORMS=vec2mat(sum((Xi-Xj).^2,1),N);
-epsilon=1200;
-K=exp(-NORMS/epsilon);
-D=diag(K*ones(N,1));
-A=(inv(D))*K;
-[EigVec, EigVal] = eig(A);
-t=1;
-psi=(EigVal.^t)*(EigVec');
-figure;
-scatter3(EigVec(:,2),EigVec(:,3),EigVec(:,4));
-figure;
-scatter(EigVec(:,2),EigVec(:,3));
 
+N        = 1000;
+lable2   = find(labels == 2, N);
+s_images = images(:,lable2);
+
+display_network(s_images(:,1:100)); % Show the first 100 images
+
+%%
+mDist = squareform( pdist(s_images') );
+% figure; imagesc(M); colorbar;
+
+epsilon = 1 * median(mDist(:));
+K       = exp(-mDist.^2 / epsilon.^2);
+A       = bsxfun(@rdivide, K, sum(K, 2));
+
+[EigVec, EigVal] = eig(A);
+
+%%
+figure; scatter3(EigVec(:,2), EigVec(:,3), EigVec(:,4), 100, 1:N, 'Fill'); colorbar;
+
+%%
+
+% t=1;
+% psi=(EigVal.^t)*(EigVec');
+% figure;
+% scatter3(EigVec(:,2),EigVec(:,3),EigVec(:,4));
+% figure;
+% scatter(EigVec(:,2),EigVec(:,3));
+% 
+% f = @(x,y) x*y
+% f(4, 3)
