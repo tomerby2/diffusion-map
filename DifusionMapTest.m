c0dbebe4077929ac0914b40a1639363a2dd569a8
tomerby2 <tomerby2@gmail.com>
2017-04-02 16:32:38 +0300

    starting add pictures

--- DifusionMapTest.m
+++ DifusionMapTest.m
@@ -10,7 +10,7 @@ s_images = images(:,lable2);
 
 display_network(s_images(:,1:100)); % Show the first 100 images
 
-%%
+%% finding eigenvectors
 mDist = squareform( pdist(s_images') );
 % figure; imagesc(M); colorbar;
 
@@ -20,8 +20,18 @@ A       = bsxfun(@rdivide, K, sum(K, 2));
 
 [EigVec, EigVal] = eig(A);
 
-%%
+%% plotting
 figure; scatter3(EigVec(:,2), EigVec(:,3), EigVec(:,4), 100, 1:N, 'Fill'); colorbar;
+title('diffusion map');
+xlabel('\psi_2');
+ylabel('\psi_3');
+zlabel('\psi_4');
+
+xImage = [EigVec(1,2) EigVec(1,2); EigVec(1,2) EigVec(1,2)];
+yImage = [EigVec(1,3)+50 EigVec(1,3)+50; EigVec(1,3)-50 EigVec(1,3)-50];
+zImage = [EigVec(1,4)-50 EigVec(1,4)+50; EigVec(1,4)-50 EigVec(1,4)+50];
+figure; surf(xImage,yImage,zImage,'CData',vec2mat(s_images(:,1),28),...
+     'FaceColor','texturemap');
 
 %%
 
