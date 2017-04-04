05915184dad1ad5787ae6b88d15a084d25863f10
tomerby2 <tomerby2@gmail.com>
2017-04-04 20:41:40 +0300

    show example pictures of the digit-2 for psi2 & psi3

--- DifusionMapTest.m
+++ DifusionMapTest.m
@@ -5,6 +5,8 @@ images = loadMNISTImages('train-images.idx3-ubyte');
 labels = loadMNISTLabels('train-labels.idx1-ubyte');
 
 N        = 1000;
+
+
 lable2   = find(labels == 2, N);
 s_images = images(:,lable2);
 
@@ -20,27 +22,29 @@ A       = bsxfun(@rdivide, K, sum(K, 2));
 
 [EigVec, EigVal] = eig(A);
 
-%% plotting
+%% plotting scatter 3D
 figure; scatter3(EigVec(:,2), EigVec(:,3), EigVec(:,4), 100, 1:N, 'Fill'); colorbar;
 title('diffusion map');
 xlabel('\psi_2');
 ylabel('\psi_3');
 zlabel('\psi_4');
 
-xImage = [EigVec(1,2) EigVec(1,2); EigVec(1,2) EigVec(1,2)];
-yImage = [EigVec(1,3)+50 EigVec(1,3)+50; EigVec(1,3)-50 EigVec(1,3)-50];
-zImage = [EigVec(1,4)-50 EigVec(1,4)+50; EigVec(1,4)-50 EigVec(1,4)+50];
-figure; surf(xImage,yImage,zImage,'CData',vec2mat(s_images(:,1),28),...
-     'FaceColor','texturemap');
 
-%%
+%% show example for psi2 & psi3
 
-% t=1;
-% psi=(EigVal.^t)*(EigVec');
-% figure;
-% scatter3(EigVec(:,2),EigVec(:,3),EigVec(:,4));
-% figure;
-% scatter(EigVec(:,2),EigVec(:,3));
-% 
-% f = @(x,y) x*y
-% f(4, 3)
+figure;
+for i=1:N
+    if( abs(EigVec(i,3))<0.005 || abs(EigVec(i,2))<0.005)
+        xImage = [EigVec(i,2)-0.001 EigVec(i,2)-0.001; EigVec(i,2)+0.001 EigVec(i,2)+0.001];
+        yImage = [EigVec(i,3)+0.001 EigVec(i,3)-0.001; EigVec(i,3)+0.001 EigVec(i,3)-0.001];
+        zImage = [EigVec(i,4) EigVec(i,4); EigVec(i,4) EigVec(i,4)];
+        surf(xImage,yImage,zImage,'CData',vec2mat(s_images(:,i),28),...
+          'FaceColor','texturemap');
+        hold on;
+    end
+end
+ title('sampels of pictures for the first 2 eig-vecs');
+xlabel('\psi_2');
+ylabel('\psi_3');
+zlabel('\psi_4');
+
